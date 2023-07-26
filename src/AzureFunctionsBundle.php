e6bed9fc71839344448dbf1274c478c88f5eaeb8
Ben Bleach <ben.bleach@xiifactors.com>
2023-07-26 16:29:07 +0100

    wip

---
+++ src/AzureFunctionsBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace XIIFactors\AzureFunctions;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class AzureFunctionsBundle extends AbstractBundle
+{
+}
