cca9c7c3562d7bdbfbddd474fbf028c01017ca53
Ben Bleach <ben.bleach@xiifactors.com>
2023-07-26 17:07:28 +0100

    wip

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace App;
+declare(strict_types=1);
+
+namespace XIIFactors\AzureFunctions;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
