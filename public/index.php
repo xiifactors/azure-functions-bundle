cf043c292e311c275beae8930d6795aa62af46da
Ben Bleach <ben.bleach@xiifactors.com>
2023-07-26 17:43:25 +0100

    wip

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__).'/../../vendor/autoload_runtime.php';
 
 $_ENV['APP_ENV'] = getenv('APP_ENV', true) ?: getenv('APP_ENV');
 $_ENV['APP_DEBUG'] = getenv('APP_DEBUG', true) ?: getenv('APP_DEBUG');
